1e6c88aa75490e80326d226251841113385cbc41
JackHaozhu <jackhaozhu@gmail.com>
2023-10-19 16:36:23 +0800

    Fixed Decorative Spawner Bugs(still need to fix shading bugs).

--- src/main/java/name/jacksdecorations/item/ModItems.java
+++ src/main/java/name/jacksdecorations/item/ModItems.java
@@ -1,6 +1,7 @@
 package name.jacksdecorations.item;
 
 import name.jacksdecorations.JacksDecorations;
+import name.jacksdecorations.block.ModBlocks;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -15,6 +16,7 @@ public class ModItems {
 
     private static void addItemsToBuildingBlocks(FabricItemGroupEntries entries) {
 //        entries.add(ModItems.EXAMPLE);
+        entries.add(ModBlocks.DECORATIVE_SPAWNER);
     }
 
     private static Item registerItem(String name, Item item) {
